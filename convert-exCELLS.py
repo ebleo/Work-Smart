1019e95aa114119696614fc340a1b747fb40fbd8
ebleo <danielle.leong@ebiquity.com>
2016-11-17 13:21:06 -0500

    FUNCTION MADE
    Allows up to 2 alpha in index [ie, AA34] and inf many integers

--- convert-exCELLS.py
+++ convert-exCELLS.py
@@ -4,34 +4,33 @@ Created on Wed Nov 16 14:55:10 2016
 
 @author: danielle.leong
 """
+def xl_ind(x):
+    index = []
+    c = 0
+    r = ''
+    add_cols = 0
 
-cN = 0
-c = 0
-r = ''
-y = []
-rTest = ''
-prev = 0
-plus = 0
-add_cols = 0
-
-x = input("cell name/ind \t")
-xlen = len(x)
-for i in range(xlen):
-    if ord('A') <= ord(x[i]) <= ord('Z'):
-        cVal = ord(x[i]) - ord('A')
-        cNext = ord('A') <= ord(x[i + 1]) <= ord('Z')
-        print(cNext)
-        if cNext:
-            print('entered')
-            print(x[i])
-            add_cols = (cVal + 1) * 26
-            print(add_cols)
-            c = c + add_cols
-            print("l1\t",c)
+    xlen = len(x)
+    for i in range(xlen):
+        if ord('A') <= ord(x[i]) <= ord('Z'):
+            cVal = ord(x[i]) - ord('A')
+            cNext = ord('A') <= ord(x[i + 1]) <= ord('Z')
+            print(cNext)
+            if cNext:
+                print('entered')
+                print(x[i])
+                add_cols = (cVal + 1) * 26
+                print(add_cols)
+                c = c + add_cols
+                print("l1\t",c)
+            else:
+                c = c + cVal + 1
+                print("l2\t",c)
         else:
-            c = c + cVal + 1
-            print("l2\t",c)
-    else:
-        r = (r + x[i])
+            r = (r + x[i])
 
-r = int(r)
+    r = int(r)
+    print(r)
+    index.append(r)
+    index.append(c)
+    return index
