af1a29075bbd47496317c6b4925e929cfbcf9823
ebleo <danielle.leong@ebiquity.com>
2016-11-17 11:49:31 -0500

    Fixed incorrect col count
    Prev version is off by some number, now corrected.
    line 22 adds iteration length for alpha, difference for alphas, and +1
    for indexing

--- convert-exCELLS.py
+++ convert-exCELLS.py
@@ -5,26 +5,28 @@ Created on Wed Nov 16 14:55:10 2016
 @author: danielle.leong
 """
 
-cN = -1
+cN = 0
 c = 0
 r = []
 y = []
 rTest = ''
+prev = 0
 
-x = input("cell name/ind")
+x = input("cell name/ind \t")
 xlen = len(x)
 for i in range(xlen):
     if ord('A') <= ord(x[i]) <= ord('Z'):
         cVal = ord(x[i]) - ord('A')
-        c += cVal
-        cN += cN
         if cN == 1:
             cN = 0
-            c = c + cN
+            c = c + 26 + (cVal - prev) + 1
+        else:
+            c += cVal
+            cN += 1
+            prev = cVal
+
     else:
         r.append(int(x[i]))
         rTest = (rTest + x[i])
-        
-rTest = int(rTest)    
-    
-        
+
+rTest = int(rTest)
